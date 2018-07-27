41c2b8edfa748e4355ed7bac01d59dc856453147
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-27 11:16:59 +0530

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,14 @@
+package janani;
+import java.util.Scanner;
+public class Counter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		System.out.println("Enter the number");
+	    Scanner s=new Scanner(System.in);
+	    n=s.nextInt();
+		System.out.println(n);
+	}
+
+}
